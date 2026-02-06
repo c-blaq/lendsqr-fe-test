4295513facfb6ad777c8b2259ebb1cb2f97c8748
c-blaq <babayejuadesina@gmail.com>
2026-02-06 11:13:03 +0100

    chore: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/index.tsx";
 import "@/styles/main.scss";
 import AppLoader from "./components/ui/Loader.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Suspense fallback={<AppLoader />}>
+      <Analytics />
       <RouterProvider router={router} />
     </Suspense>
   </StrictMode>
